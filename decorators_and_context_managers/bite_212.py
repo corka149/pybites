9be78f471b4d81ec820f97178e67749ec51c7f8b
corka149 <corka149@mailbox.org>
2020-02-24 20:45:35 +0100

    Added bite_212.py

---
+++ decorators_and_context_managers/bite_212.py
@@ -0,0 +1,14 @@
+from contextlib import suppress, nullcontext
+
+
+def sum_numbers(numbers):
+    """This generator divides each nummber by its consecutive number.
+       So if it gets passed in [4, 2, 1] it yields 4/2 and 2/1.
+       It ignores ZeroDivisionError and TypeError exceptions (latter happens
+       when a string or other non-numeric data type is in numbers)
+
+       Task: use contextlib's suppress twice to make the code below more concise.
+    """
+    for i, j in zip(numbers, numbers[1:]):
+        with suppress(TypeError, ZeroDivisionError):
+            yield i/j
